26c0962cf04e61a22ef639cb0037b05dcd8c340e
Alexander Matos <amatosolivo@gmail.com>
2018-09-10 23:42:58 -0400

    Initial application generated by JHipster-5.3.1

---
+++ src/main/java/com/cocaleca/rifado/config/FeignConfiguration.java
@@ -0,0 +1,18 @@
+package com.cocaleca.rifado.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.cocaleca.rifado")
+public class FeignConfiguration {
+
+    /**
+     * Set the Feign specific log level to log client REST requests
+     */
+    @Bean
+    feign.Logger.Level feignLoggerLevel() {
+        return feign.Logger.Level.BASIC;
+    }
+}
